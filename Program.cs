b382994f23cdde2c5636e3bbcb4b28d027173919
Vytautas Skergelza <skvy19>
2019-11-11 18:00:20 +0100

    skapade huvudmeny

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace Multifabriken
     {
         static void Main(string[] args)
         {
+            Console.Clear();
+            Console.WriteLine("**********************************************\n");
+            Console.WriteLine("\t\tMultifabriken AB");          
+            Console.WriteLine("\n**********************************************");  
+            Console.WriteLine("[1] Bilar\t\t[5]Lista produkter\n[2] Godis\t\t[6]Avsluta\n[3] Rör\n[4] Havremjölk");
+            Console.WriteLine("______________________________________________\n");       
         }
     }
 }
