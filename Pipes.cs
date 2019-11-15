2d798428c25ff4019de06131ba6b70d4d16d253b
Vytautas Skergelza <skvy19>
2019-11-15 19:27:55 +0100

    test och inputhantering

--- Pipes.cs
+++ Pipes.cs
@@ -7,9 +7,43 @@ class Pipes
         public int diameter;
         public int length;
 
+        public void Form(string input) {
+
+            Console.WriteLine("Beställning av rör\n");
+
+            do {
+                Console.Write("Ange diameter i mm (endast heltal!): ");
+                input = Console.ReadLine();
+                if (isValidInput(input, diameter)) {
+                    diameter = Convert.ToInt32(input);
+                } else {
+                    Console.WriteLine("Ogiltigt värde!");
+                }
+            } while (!isValidInput(input, diameter));
+
+            do {
+                Console.Write("Ange längd i mm (endast heltal!): ");
+                input = Console.ReadLine();
+                if (isValidInput(input, length)) {
+                    length = Convert.ToInt32(input);
+                } else {
+                    Console.WriteLine("Ogiltigt värde!");
+                }
+            } while (!isValidInput(input, length));
+
+        }
+
         public void Confirmation()
         {
-            Console.WriteLine("Rör i {0}mm diameter och {1}mm längd har beställts!", diameter, length);
+            Console.WriteLine("\nRör i {0}mm diameter och {1}mm längd har beställts!", diameter, length);
+        }
+
+        public string Item() {
+            return string.Format("Rör i {0}mm diameter och {1}mm längd.", diameter, length);
+        }
+
+        public bool isValidInput(string input, int value) {
+            return (!string.IsNullOrWhiteSpace(input) && int.TryParse(input, out value)) ? true : false;
         }
     }
 }
--- Sweets.cs
+++ Sweets.cs
@@ -7,9 +7,46 @@ class Sweets
         public string taste;
         public int quantity;
 
+        public void Form(string input) {
+
+            Console.WriteLine("Beställning av godis\n");
+
+            do {
+                Console.Write("Ange smak: ");
+                input = Console.ReadLine();
+                if (isValidInputForTaste(input)) {
+                    taste = input;
+                } else {
+                    Console.WriteLine("Ogiltigt värde!");
+                }
+            } while (!isValidInputForTaste(input));
+
+            do {
+                Console.Write("Ange antal: ");
+                input = Console.ReadLine();
+                if (isValidInputForQty(input, quantity)) {
+                    quantity = Convert.ToInt32(input);
+                } else {
+                    Console.WriteLine("Ogiltigt värde!");
+                }
+            } while (!isValidInputForQty(input, quantity));
+
+        }
+
         public void Confirmation()
         {
-            Console.WriteLine("{0}st godis i {1} smak har beställts!", quantity, taste);
+            Console.WriteLine("\n{0}st godis i {1} smak har beställts!", quantity, taste);
+        }
+
+        public string Item() {
+            return string.Format("{0}st godis i {1} smak.", quantity, taste);
+        }
+
+        public bool isValidInputForTaste(string input) {
+            return (!string.IsNullOrWhiteSpace(input) && !Char.IsDigit(input[0])) ? true : false;
+        }
+        public bool isValidInputForQty(string input, int value) {
+            return (!string.IsNullOrWhiteSpace(input) && int.TryParse(input, out value)) ? true : false;
         }
     }
 }
